f20d5b9de8573d4384984933c5e861dfc4caa9e7
epiphany07 <tanubuddijyothirmayee@gmail.com>
2022-10-15 09:19:17 +0530

    updated fedblog

---
+++ ReactJS/lab/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TrafficLights from './components/TrafficLights';
+
+function App() {
+  return (
+    <div className="App">
+      <TrafficLights />
+    </div>
+  );
+}
+
+export default App;
